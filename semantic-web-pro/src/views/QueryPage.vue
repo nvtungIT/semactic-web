0833101164de3b62e9012cfbbb66a91dea62d11a
LongBG <nguyenvanlonglnbg@gmail.com>
2024-01-04 21:14:49 +0700

    Sửa hiển thị dữ liệu

--- semantic-web-pro/src/views/QueryPage.vue
+++ semantic-web-pro/src/views/QueryPage.vue
@@ -2,14 +2,24 @@
     <div class="container-query">
         <div class="content">
             <h1>Semantic web</h1>
-            <textarea class="input-query" v-model="query">
+            <textarea class="input-query" v-model="query" placeholder="Truy vấn">
 
             </textarea>
             <div class="btn" @click="excuteQuery">
                Excute Query
             </div>
-            <textarea  class="input-query" v-model="dataQuery">
-            </textarea>
+            <table>
+                <thead>
+                    <tr>
+                    <th scope="col" v-for="(header, index) in headers" :key="index">{{ header  }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(row, index) in rows" :key="index">
+                      <td class="cell" data-label="Account" v-for="(header, ind) in headers" :key="ind">{{ row[header] ? row[header].value: '' }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -21,14 +31,21 @@ import { queryData } from '../services/film'
 export default {
 setup() {
     const query = ref('')
-    const dataQuery = ref('')
+    const headers = ref([])
+    const rows = ref([])
     async function excuteQuery() {
         const response = await queryData(query.value)
-        dataQuery.value = JSON.stringify(response.results.bindings, null, 2)
+        if (response.head.vars) {
+            this.headers = response.head.vars
+        }
+        if (response.results.bindings) {
+           this.rows = response.results.bindings
+        }
     }
     return {
+        headers,
         query,
-        dataQuery,
+        rows,
         excuteQuery
     }
 }
@@ -50,6 +67,13 @@ setup() {
     border-radius: 8px;
     padding: 10px;
 }
+.data-query {
+    margin-top: 10px;
+    height: 250px;
+    width: 100%;
+    border-radius: 8px;
+    padding: 10px;
+}
 .btn {
     padding: 8px;
     width: fit-content;
@@ -60,5 +84,108 @@ setup() {
 }
 .btn:hover {
     background-color: aqua;
+}
+table {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
+}
+
+table caption {
+  font-size: 1.5em;
+  margin: .5em 0 .75em;
+}
+
+table tr {
+  background-color: #f8f8f8;
+  border: 1px solid #ddd;
+  padding: .35em;
+}
+
+table th,
+table td {
+  padding: .625em;
+  text-align: center;
+}
+
+table th {
+  font-size: .85em;
+  letter-spacing: .1em;
+  text-transform: uppercase;
+}
+
+@media screen and (max-width: 600px) {
+  table {
+    border: 0;
+  }
+
+  table caption {
+    font-size: 1.3em;
+  }
+  
+  table thead {
+    border: none;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+  
+  table tr {
+    border-bottom: 3px solid #ddd;
+    display: block;
+    margin-bottom: .625em;
+  }
+  
+  table td {
+    border-bottom: 1px solid #ddd;
+    display: block;
+    font-size: .8em;
+    text-align: right;
+  }
+  
+  table td::before {
+    /*
+    * aria-label has no advantage, it won't be read inside a table
+    content: attr(aria-label);
+    */
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  
+  table td:last-child {
+    border-bottom: 0;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* general styling */
+body {
+  font-family: "Open Sans", sans-serif;
+  line-height: 1.25;
+}
+.cell {
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
 </style>
